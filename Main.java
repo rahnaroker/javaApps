d064699b29080acef3b243f54f17ed376295561e
rahnar0ker <rahnar0ker@gmail.com>
2017-07-30 20:12:52 +0400

    bubble's sort add try-catch

--- Main.java
+++ Main.java
@@ -1,19 +1,29 @@
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         int n = 0;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Write massive's length");
-        n = scan.nextInt();
-        int[]mass = new int[n];
+        try {
 
-        for ( int i = 0; i < n; i++) {
-            System.out.println("Write next value: ");
-            mass[i] = scan.nextInt();
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Write massive's length: ");
+            n = scan.nextInt();
+            int[]mass = new int[n];
+
+            for ( int i = 0; i < n; i++) {
+                System.out.println("Write next value: ");
+                mass[i] = scan.nextInt();
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Wrong data type");
         }
+
     }
+
+
 }
