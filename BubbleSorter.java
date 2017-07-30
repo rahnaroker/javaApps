572bff3d6ff982c7b48c71379f26003d7a2aeff7
rahnar0ker <rahnar0ker@gmail.com>
2017-07-30 21:17:00 +0400

    bubble's sort fin

---
+++ BubbleSorter.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class BubbleSorter {
+    public static <T extends Comparable<T>> void sort(T[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
+                    T tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                    swapped = true;
+                }
+            }
+            if(!swapped)
+                break;
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -7,23 +7,21 @@ public class Main {
 
     public static void main(String[] args) {
         int n = 0;
-        try {
-
-            Scanner scan = new Scanner(System.in);
+        try (Scanner scan = new Scanner(System.in)) {
             System.out.println("Write massive's length: ");
             n = scan.nextInt();
-            int[]mass = new int[n];
-
+            Integer[]arr = new Integer[n];
             for ( int i = 0; i < n; i++) {
                 System.out.println("Write next value: ");
-                mass[i] = scan.nextInt();
+                arr[i] = scan.nextInt();
             }
-
+            BubbleSorter.sort(arr);
+            System.out.print("Sorted array: ");
+            for (Integer i : arr ) {
+                System.out.print(i + " ");
+            }
         } catch (InputMismatchException e) {
             System.out.println("Wrong data type");
         }
-
     }
-
-
 }
