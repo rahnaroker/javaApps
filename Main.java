93182abbd76ab2174c1b73504e04f56f140e4947
rahnar0ker <rahnar0ker@gmail.com>
2017-07-30 17:48:42 +0400

    bubble's sort first step

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.print("Hello, World!");
+        int n = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Write massive's length");
+        n = scan.nextInt();
+        int[]mass = new int[n];
+
+        for ( int i = 0; i < n; i++) {
+            System.out.println("Write next value: ");
+            mass[i] = scan.nextInt();
+        }
     }
 }
